208a44b2fe6099544649dc901a4d814b8b06efc5
oumaima <omaimaelbdraouy@gmail.com>
2025-08-13 12:35:19 +0100

    Commit

--- week1/day3/Dailychallenge/daily_challenge.py
+++ week1/day3/Dailychallenge/daily_challenge.py
@@ -1,6 +1,9 @@
 # Daily challenge: Old MacDonald’s Farm
 
 # Step 1: Create the Farm Class
+from email.mime import text
+
+
 class Farm:
     # Step 2: Implement the __init__ Method
     def __init__(self,farm_name):
@@ -16,22 +19,33 @@ class Farm:
     def get_info(self):
         text = f"Welcome to {self.farm_name} farm! , we have "
         for animal, count in self.animals.items():
-            text += f"{count} {animal}(s), E-I-E-I-O!"
+            text += f"{count} {animal}(s), E-I-E-I-O! "
         text = text.rstrip(", ") + "."
         return text
     def get_animal_types (self):
         result= print(sorted(list(self.animals.keys())))
         return result
-    def get_short_info (self):
-        text= print(f"{self.farm_name} farm has {', '.join(self.animals)}.")
+    def get_short_info(self):
+    # Filter les animaux li count dyalhom > 1
+        filtered_animals = [f"{count} {animal}s" for animal, count in self.animals.items() if count > 1]
+
+    # Build l-text
+        if filtered_animals:
+           text = f"{self.farm_name} farm has " + ", ".join(filtered_animals) + "."
+        else:
+           text = f"{self.farm_name} farm has no animals with more than one count."
+
+        print(text)
         return text
+
 
 
 # Example usage step5
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow', 5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
+macdonald.add_animal('sheep', 3)
+macdonald.add_animal('sheep', 3)
+macdonald.add_animal('sheep', 1)
 macdonald.add_animal('goat', 12)
 print(macdonald.get_info())
 
