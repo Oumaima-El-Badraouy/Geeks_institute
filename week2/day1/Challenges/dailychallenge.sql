38595c10ed3cf50d367a0f158382814320b508ed
oumaima <omaimaelbdraouy@gmail.com>
2025-08-17 00:10:31 +0100

    commit

---
+++ week2/day1/Challenges/dailychallenge.sql
@@ -0,0 +1,5 @@
+select count(*) from actors;
+INSERT INTO actors (first_name, last_name, birth_date) VALUES
+( 'ElBadraouy', '2005-12-08'); 
+-- rayw93 error hit khasou wahd lfield sinon ladrnah haka INSERT INTO actors (first_name, last_name, birth_date) VALUES
+-- ( '','ElBadraouy', '2005-12-08'); raykhdm
