34733101c2044be91dea03ec5e1ce69fbf93844e
oumaima <omaimaelbdraouy@gmail.com>
2025-08-16 20:54:51 +0100

    commit

---
+++ week2/day1/Exercises/ExerciseXPGold.sql
@@ -0,0 +1,28 @@
+-- Exercise 1 : Students table #2
+/*qst1*/
+create database bootcamp;
+use bootcamp;
+/*qst2 and 3*/
+create table students(
+id SERIAL primary key,
+first_name varchar(10),
+last_name varchar(10),
+birth_date date
+);
+
+INSERT INTO students (first_name, last_name, birth_date) VALUES
+('Marc', 'Benichou', '1998-11-02'),
+('Yoan', 'Cohen', '2010-12-03'),
+('Lea', 'Scott', '1987-07-27'),
+('Amelia', 'Green', '1996-04-07'),
+('David', 'Johnson', '2003-06-14'),
+('Omer', 'Simpson', '1980-10-03');
+
+INSERT INTO students (first_name, last_name, birth_date) VALUES
+('Oumaima', 'ElBadraouy', '2005-12-08');
+
+select * from students;
+select first_name, last_name,birth_date from students   Order by  first_name asc limit 5;
+select first_name, last_name,birth_date from students Order by birth_date desc limit 1;
+select first_name, last_name,birth_date from students limit 2 offset 2;
+
