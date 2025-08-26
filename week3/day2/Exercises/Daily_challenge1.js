4bb996ae8204a633fc042ca233bf2f82e3271d3f
oumaima <omaimaelbdraouy@gmail.com>
2025-08-26 17:17:13 +0100

    Commit the week3

---
+++ week3/day2/Exercises/Daily_challenge1.js
@@ -0,0 +1,8 @@
+// solutions
+let sentence = "The movie is not that bad, I like it";
+let index_not = sentence.indexOf("not");
+let index_bad = sentence.indexOf("bad");
+if (index_not !== -1 && index_bad !== -1 && index_not < index_bad) {
+     sentence = sentence.slice(0, index_not) + "good" + sentence.slice( index_bad + 3) ;
+}
+console.log(sentence);
