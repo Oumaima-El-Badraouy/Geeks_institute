f78ad0abf310313b67ef2f7b80bd4a2127b81617
oumaima <omaimaelbdraouy@gmail.com>
2025-08-30 22:36:17 +0100

    Commit exercise XP

---
+++ week3/day4/Exercises/ExercisesXP.js
@@ -0,0 +1,38 @@
+// Exercise1
+ //Part I
+            function sayHello(){
+               
+                 window.document.body.innerHTML = "<h1>Hello World</h1>";
+            }
+          // let timer=setTimeout(sayHello,2000); 
+            //Part II
+            function sayHello2(){
+                let p=document.getElementById('container');
+                let child=document.createElement('p');
+                child.textContent="hello world";
+                p.appendChild(child);
+
+            }
+             let timer=setTimeout(sayHello2,2000);
+            //Part III
+         let container = document.getElementById('container');
+let button = document.getElementById('clear');
+
+function addParagraph() {
+    let p = document.createElement('p');
+    p.textContent = "Hello World";
+    container.appendChild(p);
+
+    if (container.children.length >= 5) {
+        clearInterval(timer2);
+    }
+}
+
+let timer2 = setInterval(addParagraph, 2000);
+
+button.addEventListener('click', function() {
+    clearInterval(timer2);
+});
+       
+
+//  Exercise 2 : Move the box
