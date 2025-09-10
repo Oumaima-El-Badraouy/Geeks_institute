1c322cd1f06b4160d8a0c5b3fbcceb8095b646d6
oumaima <omaimaelbdraouy@gmail.com>
2025-09-10 17:58:44 +0100

    commit Exercises XP Ninga day3 week 4

---
+++ week4/day3/Exercises/Exercises_XP_Ninja.js
@@ -0,0 +1,17 @@
+// Exercise 1 : Bird class
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();//raytl3 lina you're pink 3ad  you're a bird hhh
+
+
