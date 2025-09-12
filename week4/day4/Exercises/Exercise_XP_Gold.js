5d2fc3707f1673faba536a574d3766a739c26e20
oumaima <omaimaelbdraouy@gmail.com>
2025-09-12 10:40:13 +0100

    commit ex xp gold week 4 day4

--- week4/day4/Exercises/Exercise_XP_Gold.js
+++ week4/day4/Exercises/Exercise_XP_Gold.js
@@ -1,3 +1,52 @@
 // Exercise 1 : HTML Form #3
+// file index.html
 
 
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>form</title>
+// </head>
+// <body>
+//     <form method="Get" action="data.html">
+//         <label for="username">Votre nom :</label>
+//         <input type="text" name="username" id="username">
+//         <label for="prenom">Votre prenom :</label>
+//         <input type="text" name="prenom" id="prenom">
+//         <input type="submit" value="Send">
+
+//     </form>
+
+// </body>
+// </html>
+
+
+
+// file data.html
+
+
+// <!doctype html>
+// <html>
+// <head><meta charset="utf-8"><title>Receiver GET</title></head>
+// <body>
+//   <h2>Received (GET)</h2>
+//   <div id="output">Loading…</div>
+
+//   <script>
+//     const params = new URLSearchParams(window.location.search);
+//     const firstName = params.get('username') || '';
+//     const lastName = params.get('prenom') || '';
+//     const out = document.getElementById('output');
+//     if (!firstName && !lastName && !email){
+//       out.textContent = 'No data received.';
+//     } else {
+//       out.innerHTML = `
+//         <p><strong>First name:</strong> ${firstName}</p>
+//         <p><strong>Last name:</strong> ${lastName}</p>
+//       `;
+//     }
+//   </script>
+// </body>
+// </html>
