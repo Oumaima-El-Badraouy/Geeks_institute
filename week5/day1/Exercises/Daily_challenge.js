f17fea9d6cd978cc79cfc3060023efd1eceabd19
oumaima <omaimaelbdraouy@gmail.com>
2025-09-16 17:11:29 +0100

    commit  daily challenge week 5 day1

--- week5/day1/Exercises/Daily_challenge.js
+++ week5/day1/Exercises/Daily_challenge.js
@@ -30,3 +30,94 @@ makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
       .then((arr) => sortWords(arr))
       .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
       .catch(error => console.log(error))
+
+
+
+// 2nd daily challenge
+const morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`;
+function toJs(morse) {
+    return new Promise((resolve, reject) => {
+        try {
+            if (!morse) throw new Error("No Morse code provided");  
+            const json = JSON.parse(morse);
+            resolve(json);
+        } catch (error) {
+            reject("Invalid Morse code");
+        }
+    });
+}
+function toMorse(morseObj) {
+    return new Promise((resolve, reject) => {
+        const str = prompt("Enter a string to convert to Morse code: ");
+        if (!morseObj || Object.keys(morseObj).length === 0) {
+            reject("Invalid Morse object");
+            return;
+        }
+        if (!str || typeof str !== "string") {
+            reject("Invalid input string");
+            return;
+        }
+        if(str.split("").some(char => !morseObj[char])) {
+            reject("String contains unsupported characters");
+            return;
+        }
+        const morseCode = str.split("").map(char => morseObj[char]).join(" ");
+        resolve(morseCode);
+    });
+}
+function joinWords(morseTranslation){
+    return new Promise((resolve, reject) => {
+        if(!morseTranslation || typeof morseTranslation !== "string"){
+            reject("Invalid Morse translation");
+            return;
+        }
+        const words = morseTranslation.split(" ").map(char => morseObj[char]).join("\n");
+        resolve(words);
+    });
+}
