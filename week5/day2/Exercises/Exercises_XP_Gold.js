1c534b2d01de4b9af34bba5c6602d3aac0888683
oumaima <omaimaelbdraouy@gmail.com>
2025-09-17 21:13:04 +0100

    day2 week5

--- week5/day2/Exercises/Exercises_XP_Gold.js
+++ week5/day2/Exercises/Exercises_XP_Gold.js
@@ -1,19 +1,124 @@
-// Exercise 1 : Giphy API #2
-const giphy=async()=>{
-    try{
-        const response=await fetch("https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
-         if(!response.ok){
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data=await response.json();
-        const randomnumber=Math.floor(Math.random()*data.data.length);
-        const gif=data.data[randomnumber].images.original.url;
-        const img= document.createElement("img");
-        img.src=gif;
-        document.body.appendChild(img);
-    }
-    catch(error){
-        console.error("Error fetching data from Giphy API:", error);
-    }
+// // Exercise 1 : Giphy API #2
+// const giphy=async()=>{
+//     try{
+//         const response=await fetch("https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+//          if(!response.ok){
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         const data=await response.json();
+//         const randomnumber=Math.floor(Math.random()*data.data.length);
+//         const gif=data.data[randomnumber].images.original.url;
+//         const img= document.createElement("img");
+//         img.src=gif;
+//         document.body.appendChild(img);
+//     }
+//     catch(error){
+//         console.error("Error fetching data from Giphy API:", error);
+//     }
+// }
+// giphy();
+
+// // Exercise 2 : Analyze #2
+// let resolveAfter2Seconds = function () {
+//     console.log("starting slow promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("slow");
+//             console.log("slow promise is done");
+//         }, 2000);
+//     });
+// };
+
+// let resolveAfter1Second = function () {
+//     console.log("starting fast promise");
+//     return new Promise(resolve => {
+//         setTimeout(function () {
+//             resolve("fast");
+//             console.log("fast promise is done");
+//         }, 1000);
+//     });
+// };
+
+// let sequentialStart = async function () {
+//     console.log('==SEQUENTIAL START==');
+//     const slow = await resolveAfter2Seconds();
+//     console.log(slow);
+//     const fast = await resolveAfter1Second();
+//     console.log(fast);
+// }
+
+// sequentialStart()
+
+// radi tla3 hady lwla '==SEQUENTIAL START==' puis "starting slow promise"puis "slow promise is done"  3ad 
+//"slow" puis "starting fast promise"puis "fast promise is done" puis'fast'
+
+
+// Exercise 3 : Analyze #3
+let resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("slow");
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+let resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("fast");
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+let concurrentStart = async function () {
+    console.log('==CONCURRENT START with await==');
+    const slow = resolveAfter2Seconds();
+    const fast = resolveAfter1Second();
+    console.log(await slow);
+    console.log(await fast);
 }
-giphy();
+
+setTimeout(concurrentStart, 4000)
+//ntsnaww 4s puis n3ayet lconcurrentStart rady ytl3 lina '==CONCURRENT START with await=='puis
+// starting slow promise
+// starting fast promise
+// slow promise is done
+// slow
+// fast promise is done
+// fast
+
+
+// Exercise 4 : Modify fetch with Async/Await
+
+const urls = [
+        "https://jsonplaceholder.typicode.com/WRONG_URL",
+        "https://jsonplaceholder.typicode.com/posts",
+        "https://jsonplaceholder.typicode.com/albums"
+      ];
+
+const getData = async function() {
+    try{
+ const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+      fetch(url).then(resp => {
+          if (!resp.ok) {
+              throw new Error(`oops error! status: ${resp.status} for ${url}`);
+          }
+          return resp.json();
+      })
+  ));
+  console.log('users', users);
+  console.log('posts', posts);
+  console.log('albums', albums);
+    }
+ catch(error){
+    console.log("oops", error); 
+}
+  
+}
+
+getData()
+
