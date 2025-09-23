8da27483f0ca88d0c241b59fa4d12f97ec117eee
oumaima <omaimaelbdraouy@gmail.com>
2025-09-23 16:41:22 +0100

    ex3 week5 day5

--- week5/day5/Exercises/Exercise3/crud-api/data/app.js
+++ week5/day5/Exercises/Exercise3/crud-api/data/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const dataService = require('./dataService');
 const app=express();
 dataService.fetchPosts().then(posts=>{
-    console.log(posts);
+    console.log("nice");
 });
 app.listen(5000,()=>{
     console.log("Server started at port 5000");
