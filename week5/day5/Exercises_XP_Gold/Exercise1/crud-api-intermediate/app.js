dec242604436ee231ac83a487bf327647df161d3
oumaima <omaimaelbdraouy@gmail.com>
2025-09-24 12:20:54 +0100

    XP challenge week5 day5

--- week5/day5/Exercises_XP_Gold/Exercise1/crud-api-intermediate/app.js
+++ week5/day5/Exercises_XP_Gold/Exercise1/crud-api-intermediate/app.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+app.use(express.json());
 app.get('/api/posts',async(req,res)=>{
-    
+    try{
+    const posts=await  axios.get('https://jsonplaceholder.typicode.com/posts');
+        res.json(posts.data);
+    }
+    catch(error){
+        console.log('error :',error.message);
+    }
+
 });
 
 app.get('/api/posts/:id',async(req,res)=>{
+     try{
+        const id=req.params.id;
+    const post=await  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+        res.json(post.data);
+    }
+    catch(error){
+        console.log('error :',error.message);
+    }
 });
 app.post('/api/posts',async(req,res)=>{
+     try{
+        const data=req.body;
+    const posts= await  axios.post('https://jsonplaceholder.typicode.com/posts',data);
+        res.json('nice');
+    }
+    catch(error){
+        console.log('error :',error.message);
+    }
 });
 app.put('/api/posts/:id',async(req,res)=>{
+     try{
+        const id=req.params.id;
+        const data=req.body;
+    const posts=await  axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`,data);
+        res.json('user updated');
+    }
+    catch(error){
+        console.log('error :',error.message);
+    }
 });
 app.delete('/api/posts/:id',async(req,res)=>{
-});
+     try{
+        const id = req.params.id;
 
+    const posts=await  axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        res.json('user deleted ');
+    }
+    catch(error){
+        console.log('error :',error.message);
+    }
+});
 app.listen(5000, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
+  console.log(`Server running: http://localhost:5000`);
 });
