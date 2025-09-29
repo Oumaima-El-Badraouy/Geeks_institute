af9fd83c57c2bb578a613882e74d36a077169941
oumaima <omaimaelbdraouy@gmail.com>
2025-09-29 12:01:53 +0100

    day1 week7

--- week7/day1/ExercisesXP/Exercise1/day1XP/exercises/src/DemoCarousel.jsx
+++ week7/day1/ExercisesXP/Exercise1/day1XP/exercises/src/DemoCarousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the CSS
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 import { Carousel } from 'react-responsive-carousel';
-
 class DemoCarousel extends React.Component {
    render() {
        return (
