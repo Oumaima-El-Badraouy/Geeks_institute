b9a5b99834a4b7b4ec4a3872dd95b12336f95c3a
oumaima <omaimaelbdraouy@gmail.com>
2025-10-19 14:49:33 +0100

    day4 week8

--- week8/day4/dailychallenge/src/App.jsx
+++ week8/day4/dailychallenge/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import AgeDisplay from "./components/AgeDisplay";
+import AgeControls from "./components/AgeControls";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "linear-gradient(135deg, #fdfdfdff, #ecececff)",
+      }}
+    >
+      <div
+        style={{
+          background: "#fff",
+          padding: "40px",
+          borderRadius: "12px",
+          boxShadow: "0 6px 20px rgba(0,0,0,0.1)",
+          width: "350px",
+          textAlign: "center",
+        }}
+      >
+        <h1 style={{ color: "#333" }}>Age Tracker</h1>
+        <AgeDisplay />
+        <AgeControls />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
